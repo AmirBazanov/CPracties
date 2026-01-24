139188d19c777e2a654d363ff277944679280d1a
AmirBazanov <flash5651311@gmail.com>
2026-01-24 22:21:20 +0500

    ex replace \t to spaces done

--- detab.c
+++ detab.c
@@ -1,46 +1,53 @@
-#include <stdio.h>
-#define N 4
-#define LIM 1000
-void getLine(char line[]);
-void replaseTabs(char f[], char t[]);
-int main() {
-  char string[LIM];
-  char tabLesArr[LIM];
-  for (int a = 0; a < LIM; a++) {
-    string[a] = 0;
 
-    tabLesArr[a] = 0;
-  }
+#include <stdio.h>
+
+#define N 8      // количество пробелов для таба
+#define LIM 1000 // максимальный размер исходной строки
+
+void getLine(char line[]);
+void replaceTabs(char from[], char to[]);
+
+int main() {
+  char string[LIM] = {0};
+  char tabLessArr[LIM * N] = {0}; // буфер больше, чтобы вместить все пробелы
+
   getLine(string);
-  replaseTabs(string, tabLesArr);
-  for (int i = 0; i < LIM - 1; i++) {
-    putchar(tabLesArr[i]);
+  replaceTabs(string, tabLessArr);
+
+  // выводим строку до конца '\0'
+  for (int i = 0; tabLessArr[i] != '\0'; i++) {
+    putchar(tabLessArr[i]);
   }
+
+  return 0;
 }
 
 void getLine(char s[]) {
   int c, i;
   for (i = 0; i < LIM - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
     s[i] = c;
+
   if (c == '\n') {
     s[i] = c;
     ++i;
   }
+
   s[i] = '\0';
 }
 
-void replaseTabs(char from[], char to[]) {
-  int j = 0;
-  for (int i = 0; i < LIM - 1; i++) {
-    if (from[i] == '\t') {
-      for (j = i; j <= i + N || (i + N) > LIM; j++) {
-        to[j] = ' ';
-      }
-    }
-    if (j < LIM - 1) {
+void replaceTabs(char from[], char to[]) {
+  int i, j = 0;
 
-      to[j] = from[i];
-      ++j;
+  for (i = 0; from[i] != '\0'; i++) {
+    if (from[i] == '\t') {
+      // вставляем N пробелов
+      for (int a = 0; a < N; a++) {
+        to[j++] = ' ';
+      }
+    } else {
+      to[j++] = from[i];
     }
   }
+
+  to[j] = '\0';
 }
